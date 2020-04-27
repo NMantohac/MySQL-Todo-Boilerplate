ec784d3cdaa2008e200a45639007a31b8ba641f8
Norberto Mantohac <nmantohac82098@gmail.com>
2020-04-27 11:49:41 -0700

    create front end

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 9000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 // Any route that goes to slash,
 // Have the router object inside of routes
 // Handle the routing for us
